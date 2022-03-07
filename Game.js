7f5d5adc7b9c7fa976c056643cd9ce9d0a235188
Nick Orlov <ialwaysreply@gmail.com>
2022-03-07 00:08:02 -0700

    Add evaluateFancy method

--- Game.js
+++ Game.js
@@ -13,6 +13,8 @@ class Game {
     console.log('this is the game type ', gameType)
     if (gameType === 'classic') {
       this.choices = ['AKo', '22', 'JTs']
+    } else {
+      this.choices = ['AKo', '22', 'JTs', 'BJoker', 'RJoker']
     }
       this.gameType = gameType;
       console.log('this is this.choices ', this.choices)
@@ -32,7 +34,35 @@ class Game {
         return this.player1Wins++;
     } else {
       this.winner = this.player2.name;
-      return this.player2Wins ++;
+      return this.player2Wins++;
     }
   }
+
+  evaluateFancy(player1Hand, player2Hand) {
+    if (player1Hand === player2Hand) {
+      return this.winner = 'tie' 
+    } else if (
+      (player1Hand === 'AKo' && (player2Hand === 'JTs' || player2Hand === 'BJoker')) ||
+      (player1Hand === '22' && (player2Hand === 'AKo' || player2Hand === 'RJoker')) ||
+      (player1Hand === 'JTs' && (player2Hand === '22' || player2Hand === 'BJoker')) ||
+      (player1Hand === 'BJoker' && (player2Hand === '22' || player2Hand === 'RJoker')) ||
+      (player1Hand === 'RJoker' && (player2Hand === 'AKo' || player2Hand === 'JTs'))
+    ) {
+      this.winner = this.player1.name;
+      console.log('Human Won Fancy!@!!')
+      return this.player1Wins++;
+    } else {
+      this.winner = this.player2.name;
+      return this.player2Wins++;
+    }
+  }
+  resetGame() {
+    this.winner = '';
+  }
 }
+
+// AK (rock)> JTs && BJ
+// 22 (paper)> AK && RJ
+// JTs (scissors) > 22 && BJ
+// BJ (alien)> 22 && RJ
+// RJ (hard on)> Ak && JTs
